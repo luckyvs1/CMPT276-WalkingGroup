c2ff8bdbd67f0af6eb0317882dbcf8580353b097
Lucky <25218389+luckyvs1@users.noreply.github.com>
2018-06-28 00:09:16 -0700

    Add user login server logic
    Get proxy token on login and switch activity to dashboard

--- app/src/main/java/olive/walkinggroup/app/MainActivity.java
+++ app/src/main/java/olive/walkinggroup/app/MainActivity.java
@@ -3,21 +3,30 @@ package olive.walkinggroup.app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import olive.walkinggroup.R;
 import olive.walkinggroup.dataobjects.User;
+import olive.walkinggroup.proxy.ProxyBuilder;
+import olive.walkinggroup.proxy.WGServerProxy;
+import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
     private User user;
+    private WGServerProxy proxy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Build the server proxy
+        proxy = ProxyBuilder.getProxy(getString(R.string.apikey), null);
 
         user = User.getInstance();
 
@@ -33,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 // Check if the user email and password are valid
+                setUserDetails();
+
+                // Register for token received:
+                ProxyBuilder.setOnTokenReceiveCallback(token -> onReceiveToken(token));
+
+                  // Make call
+                Call<Void> caller = proxy.login(user);
+                ProxyBuilder.callProxy(MainActivity.this, caller, returnedNothing -> response(returnedNothing));
 
                 // Go to the dashboard activity
                 Intent intent = new Intent(MainActivity.this, DashBoardActivity.class);
@@ -44,6 +61,40 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Failed Login", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // Handle the token by generating a new Proxy which is encoded with it.
+    private void onReceiveToken(String token) {
+        // Replace the current proxy with one that uses the token!
+        Log.w("User logged in", "   --> NOW HAVE TOKEN: " + token);
+        proxy = ProxyBuilder.getProxy(getString(R.string.apikey), token);
+    }
+
+    // Login actually completes by calling this; nothing to do as it was all done
+    // when we got the token.
+    private void response(Void returnedNothing) {
+        notifyUserViaLogAndToast("Server replied to login request (no content was expected).");
+    }
+
+    // Put message up in toast and logcat
+    private void notifyUserViaLogAndToast(String message) {
+        Log.w("User logged in", message);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    // Get the user details from the login activity
+    private void setUserDetails() {
+        String email = getUserInput(R.id.txtGetEmail);
+        String password = getUserInput(R.id.txtGetPassword);
+
+        // Update the details of the user instance
+        user.setPassword(password);
+        user.setEmail(email);
+    }
+
+    private String getUserInput(int userInputResourceID) {
+        EditText userText = (EditText) findViewById(userInputResourceID);
+        return userText.getText().toString();
     }
 
     private void setupSignupBtn() {
