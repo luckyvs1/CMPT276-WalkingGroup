c327cf6bf1bcf4d279781e5515080aa7cbbd7b57
j_ho <joshuagjho@hotmail.com>
2018-06-27 23:06:36 -0700

    Implemented simple button functionality for the create group activity.

--- app/src/main/java/olive/walkinggroup/CreateGroupActivity.java
+++ app/src/main/java/olive/walkinggroup/CreateGroupActivity.java
@@ -1,7 +1,10 @@
 package olive.walkinggroup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
@@ -9,5 +12,55 @@ public class CreateGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
+        setupDestLocationBtn();
+        setupMeetingPlaceBtn();
+
+        setupOKBtn();
+        setupCancelBtn();
+    }
+
+    private void setupMeetingPlaceBtn() {
+        Button btn = findViewById(R.id.btnMeetingPlace);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = CreateGroupMapsActivity.makeLaunchIntent(CreateGroupActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupDestLocationBtn() {
+        Button btn = findViewById(R.id.btnDestinationLocation);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = CreateGroupMapsActivity.makeLaunchIntent(CreateGroupActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void setupCancelBtn() {
+        Button btn = findViewById(R.id.btnCreateGroupCancel);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+    private void setupOKBtn() {
+        Button btn = findViewById(R.id.btnCreateGroupOK);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/olive/walkinggroup/CreateGroupMapsActivity.java
+++ app/src/main/java/olive/walkinggroup/CreateGroupMapsActivity.java
@@ -1,5 +1,7 @@
 package olive.walkinggroup;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,16 +27,6 @@ public class CreateGroupMapsActivity extends FragmentActivity implements OnMapRe
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -50,5 +42,9 @@ public class CreateGroupMapsActivity extends FragmentActivity implements OnMapRe
                 Log.i("App","Lat: " + latLng.latitude + "Long: " + latLng.longitude);
             }
         });
+    }
+
+    public static Intent makeLaunchIntent(Context context) {
+        return new Intent(context, CreateGroupMapsActivity.class);
     }
 }
