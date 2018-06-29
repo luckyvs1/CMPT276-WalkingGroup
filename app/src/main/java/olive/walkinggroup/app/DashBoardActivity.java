7add353210ad7d4bac0181d95ee72b277f72ee75
ckwan <ckw27@gmail.com>
2018-06-28 20:25:38 -0700

    Functional dashBoard buttons and created Monitor Map and CreateGroup Activity

--- app/src/main/java/olive/walkinggroup/app/DashBoardActivity.java
+++ app/src/main/java/olive/walkinggroup/app/DashBoardActivity.java
@@ -1,7 +1,10 @@
 package olive.walkinggroup.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import olive.walkinggroup.R;
 
@@ -12,10 +15,54 @@ public class DashBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
+        createToMonitorButton();
+        createToMapButton();
+        createToCreateGroupButton();
         createLogoutButton();
     }
 
+    private void createToMonitorButton() {
+        Button btn = (Button)findViewById(R.id.toMonitor);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashBoardActivity.this, MonitorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void createToMapButton() {
+        Button btn = (Button)findViewById(R.id.toMap);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashBoardActivity.this, Map.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void createToCreateGroupButton() {
+        Button btn = (Button)findViewById(R.id.toCreateGroup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashBoardActivity.this, CreateGroupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
     private void createLogoutButton() {
-        
+        Button btn = (Button) findViewById(R.id.logoutBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashBoardActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
