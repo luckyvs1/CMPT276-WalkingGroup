58d3d7e6afbd111e43cd58bf776cc6ca85391b84
ckwan <ckw27@gmail.com>
2018-06-28 20:04:41 -0700

    Create UI buttons for dashBoard

--- app/src/main/java/olive/walkinggroup/app/DashBoardActivity.java
+++ app/src/main/java/olive/walkinggroup/app/DashBoardActivity.java
@@ -11,5 +11,7 @@ public class DashBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+
     }
 }
