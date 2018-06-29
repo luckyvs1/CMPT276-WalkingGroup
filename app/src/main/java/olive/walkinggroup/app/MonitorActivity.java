431dfff92c8586753eaa9b18d4524d44801ebbf4
ckwan <ckw27@gmail.com>
2018-06-29 00:01:42 -0700

    Add in populated list View

--- app/src/main/java/olive/walkinggroup/app/MonitorActivity.java
+++ app/src/main/java/olive/walkinggroup/app/MonitorActivity.java
@@ -2,6 +2,8 @@ package olive.walkinggroup.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import olive.walkinggroup.R;
 
@@ -17,10 +19,29 @@ public class MonitorActivity extends AppCompatActivity {
     }
 
     private void populateMonitorsMe() {
+        //Create list
+        String[] userList = {"Rory \nckw27@sfu.ca","Lucky \nlvs1@sfu.ca","Josh \njha225@sfu.ca","Jeffery \njgyeung@sfu.ca; "};
+
+        //Build Adaptor
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, userList);
+
+        //Configure the list view
+        ListView list = (ListView) findViewById(R.id.monitorsMe);
+        list.setAdapter(adapter);
 
     }
 
     private void populateIMonitor() {
+        //Create list
+        String[] userList = {"Rory","Lucky","Josh","Jeffery"};
+
+        //Build Adaptor
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, userList);
+
+        //Configure the list view
+        ListView list = (ListView) findViewById(R.id.IMonitor);
+        list.setAdapter(adapter);
+
     }
 
 
