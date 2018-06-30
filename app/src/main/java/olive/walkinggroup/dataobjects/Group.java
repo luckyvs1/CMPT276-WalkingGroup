3be9adb031ea1eb8b02410b4635046fcb00a7ac8
Lucky <25218389+luckyvs1@users.noreply.github.com>
2018-06-29 17:13:42 -0700

    Add fields for group class
    Add name, leader name and description fields with getter and setter.

--- app/src/main/java/olive/walkinggroup/dataobjects/Group.java
+++ app/src/main/java/olive/walkinggroup/dataobjects/Group.java
@@ -2,6 +2,9 @@ package olive.walkinggroup.dataobjects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Store information about the walking groups.
  *
@@ -10,4 +13,39 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Group extends IdItemBase{
+
+    private String groupName;
+    private String groupLeaderName;
+    private String groupDescription;
+
+    // Group Start Point (Latitude, Longitude)
+    // TO BE IMPLEMENTED
+
+    // Group End Point (Latitude, Longitude)
+    // TO BE IMPLEMENTED
+    
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getGroupLeaderName() {
+        return groupLeaderName;
+    }
+
+    public void setGroupLeaderName(String groupLeaderName) {
+        this.groupLeaderName = groupLeaderName;
+    }
+
+    public String getGroupDescription() {
+        return groupDescription;
+    }
+
+    public void setGroupDescription(String groupDescription) {
+        this.groupDescription = groupDescription;
+    }
+
 }
