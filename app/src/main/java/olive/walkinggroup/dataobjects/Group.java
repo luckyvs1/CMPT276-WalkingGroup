75eb4fc137bf181daa9ce32786fc8b1d9ac9a4df
Lucky <25218389+luckyvs1@users.noreply.github.com>
2018-07-03 14:44:05 -0700

    Remove toString parameters for group class with the exception of groupDescription

--- app/src/main/java/olive/walkinggroup/dataobjects/Group.java
+++ app/src/main/java/olive/walkinggroup/dataobjects/Group.java
@@ -107,16 +107,13 @@ public class Group extends IdItemBase implements Serializable{
         latArray[1] = end.latitude;
         lngArray[1] = end.longitude;
     }
-    
+
+    // TO DO: Check if any more fields need to be added
     @Override
     public String toString() {
         return "Group{" +
-                "groupName='" + groupName + '\'' +
-                ", groupDescription='" + groupDescription + '\'' +
-                ", leader=" + leader +
-                ", latArray=" + Arrays.toString(latArray) +
-                ", lngArray=" + Arrays.toString(lngArray) +
-                ", members=" + members +
+                "groupDescription='" + groupDescription + '\'' +
                 '}';
     }
+
 }
