a468bfc1f63c859dc207c2381c8468fb2e0099cb
ckwan <ckw27@gmail.com>
2018-07-04 15:44:06 -0700

    Update Fetching datat from server

--- app/src/main/java/olive/walkinggroup/app/editMonitoredByUserFragment.java
+++ app/src/main/java/olive/walkinggroup/app/editMonitoredByUserFragment.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,13 +14,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.List;
+
 import olive.walkinggroup.R;
 import olive.walkinggroup.dataobjects.Model;
 import olive.walkinggroup.dataobjects.User;
+import olive.walkinggroup.proxy.ProxyBuilder;
+import retrofit2.Call;
 
 public class editMonitoredByUserFragment extends AppCompatDialogFragment {
     private Model instance = Model.getInstance();
-    private User user = instance.getCurrentUser();
+    private User currentUser = instance.getCurrentUser();
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -34,15 +39,16 @@ public class editMonitoredByUserFragment extends AppCompatDialogFragment {
             public void onClick(DialogInterface dialog, int which) {
                 EditText userEmail = getDialog().findViewById(R.id.txtInputemail);
                 String email = userEmail.getText().toString();
-
-                User newUser = new User();
-                newUser.setEmail(email);
-                newUser.setName("Bob");
-
-                if (user.checkEmailPosInList(email,user.getMonitoredByUsers())==null && !email.equals("")) {
-                    user.addToMonitoredByUsers(newUser);
-                }
-                ((MonitorActivity)getActivity()).populateMonitorsMe();
+                addUserByEmail(email);
+//                User newUser = new User();
+//
+//                newUser.setEmail(email);
+//                newUser.setName("Bob");
+//
+//                if (user.checkEmailPosInList(email,user.getMonitoredByUsers())==null && !email.equals("")) {
+//                    user.addToMonitoredByUsers(newUser);
+//                }
+//                ((MonitorActivity)getActivity()).populateMonitorsMe();
             }
         };
 
@@ -50,14 +56,14 @@ public class editMonitoredByUserFragment extends AppCompatDialogFragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                     EditText userEmail = (EditText) getDialog().findViewById(R.id.txtInputemail);
-                    String email = userEmail.getText().toString();
+                    String email = userEmail.getText().toString().replace("@","%40");
 
-                    Integer pos = user.checkEmailPosInList(email,user.getMonitoredByUsers());
-                    if (pos!= null) {
-                        User removeUser = user.getMonitoredByUsers().get(pos);
-                        user.removeFromMonitoredByUsers(removeUser);
-                    }
-                    ((MonitorActivity)getActivity()).populateMonitorsMe();
+//                    Integer pos = currentUser.checkEmailPosInList(email,currentUser.getMonitoredByUsers());
+//                    if (pos!= null) {
+//                        User removeUser = currentUser.getMonitoredByUsers().get(pos);
+//                        currentUser.removeFromMonitoredByUsers(removeUser);
+//                    }
+//                    ((MonitorActivity)getActivity()).populateMonitorsMe();
             }
         };
 
@@ -68,4 +74,39 @@ public class editMonitoredByUserFragment extends AppCompatDialogFragment {
                 .setPositiveButton("Add User",listener_add)
                 .create();
     }
+
+    private void addUserByEmail(String email) {
+        Call<User> caller = instance.getProxy().getUserByEmail(email);
+        ProxyBuilder.callProxy((MonitorActivity)getActivity(), caller, user -> addUserToList(user));
+    }
+
+    private void addUserToList(User user){
+        if (user.checkEmailPosInList(user.getEmail(),user.getMonitoredByUsers())==null && !user.getEmail().equals("")) {
+            user.addToMonitoredByUsers(user);
+            Log.d("test",user.toString());
+            Call<List<User>> caller = instance.getProxy().addToMonitoredByUsers(user.getId(),currentUser);
+            ProxyBuilder.callProxy((MonitorActivity)getActivity(),caller,listOfUsers -> refreshMonitorActivity(listOfUsers));
+        }
+
+    }
+
+    private void refreshMonitorActivity(List<User> list){
+        currentUser.setMonitoredByUsers(list);
+        //((MonitorActivity)getActivity()).populateMonitorsMe();
+    }
+
+    private void removeUserByEmail(String email){
+        Call<User> caller = instance.getProxy().getUserByEmail(email);
+        ProxyBuilder.callProxy((MonitorActivity)getActivity(),caller, user -> removeUserFromList(user));
+    }
+
+    private void removeUserFromList(User user){
+        Integer pos = user.checkEmailPosInList(user.getEmail(),user.getMonitoredByUsers());
+        if (pos!= null) {
+            User removeUser = user.getMonitoredByUsers().get(pos);
+            user.removeFromMonitoredByUsers(removeUser);
+        }
+        ((MonitorActivity)getActivity()).populateMonitorsMe();
+    }
+
 }
