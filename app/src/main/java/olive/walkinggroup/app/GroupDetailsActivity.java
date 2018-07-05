9a9ad10d81215955312455e2aba4f49ae285308a
jgyeung <jgyeung@sfu.ca>
2018-07-05 16:07:07 -0700

    Update UI for FindGroupActivity, ListGroupsActivity, SelectUserActivity and GroupDetailsActivity

--- app/src/main/java/olive/walkinggroup/app/GroupDetailsActivity.java
+++ app/src/main/java/olive/walkinggroup/app/GroupDetailsActivity.java
@@ -1,5 +1,6 @@
 package olive.walkinggroup.app;
 
+        import android.app.ActionBar;
         import android.app.Activity;
         import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
