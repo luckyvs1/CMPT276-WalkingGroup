5d24872da96eba6d9ad495188f012bc499827e5e
j_ho <joshuagjho@hotmail.com>
2018-07-10 16:30:28 -0700

    Added an auto stop uploading gps location timer after 10 minutes.

--- app/src/main/java/olive/walkinggroup/dataobjects/UploadGpsLocation.java
+++ app/src/main/java/olive/walkinggroup/dataobjects/UploadGpsLocation.java
@@ -8,7 +8,6 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import olive.walkinggroup.app.DashBoardActivity;
 import olive.walkinggroup.proxy.ProxyBuilder;
 import retrofit2.Call;
 
@@ -16,40 +15,68 @@ public class UploadGpsLocation {
     private Activity activity;
     private Model instance;
     private User user;
-    private Timer timer;
+    private Timer uploadTimer;
+    private Timer autoStopTimer;
+    private boolean hasArrivedAtDestLocation;
+
+    private static final int NUM_MS_IN_S = 1000;
+    private static final int NUM_S_IN_MIN = 60;
 
     private static final int UPLOAD_RATE_S = 30;
     private static final int UPLOAD_DELAY_S = 0;
-    private static final int NUM_MS_IN_S = 1000;
+    private static final int STOP_UPLOAD_DELAY_MIN = 10;
     private static final String TIMESTAMP_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
 
     public UploadGpsLocation(Activity activity) {
-        timer = new Timer();
+        hasArrivedAtDestLocation = false;
+        uploadTimer = new Timer();
+        autoStopTimer = new Timer();
         this.activity = activity;
         instance = Model.getInstance();
         user = instance.getCurrentUser();
     }
 
     public void start() {
-        timer.scheduleAtFixedRate(new TimerTask() {
+        uploadTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                String timeStamp = new SimpleDateFormat(TIMESTAMP_PATTERN, Locale.CANADA).format(new Date());
-                GpsLocation lastGpsLocation = new GpsLocation(1.23, 1.23, timeStamp);
-                Call<GpsLocation> caller = instance.getProxy().setLastGpsLocation(user.getId(), lastGpsLocation);
-                ProxyBuilder.callProxy(activity, caller, returnedGpsLocation -> setLastGpsLocationReturned(returnedGpsLocation));
-
+                if (hasArrivedAtDestLocation) {
+                    startAutoStopTimer();
+                }
+                uploadGpsLocationToServer();
             }
         },UPLOAD_DELAY_S, UPLOAD_RATE_S*NUM_MS_IN_S);
 
 
     }
 
+    private void startAutoStopTimer() {
+        autoStopTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                stop();
+            }
+        },STOP_UPLOAD_DELAY_MIN*NUM_S_IN_MIN*NUM_MS_IN_S);
+    }
+
+
     public void stop() {
-        timer.cancel();
+        uploadTimer.cancel();
+    }
+
+    private void uploadGpsLocationToServer() {
+        String timeStamp = new SimpleDateFormat(TIMESTAMP_PATTERN, Locale.CANADA).format(new Date());
+        GpsLocation lastGpsLocation = new GpsLocation(1.23, 1.23, timeStamp);
+        Call<GpsLocation> caller = instance.getProxy().setLastGpsLocation(user.getId(), lastGpsLocation);
+        ProxyBuilder.callProxy(activity, caller, returnedGpsLocation -> setLastGpsLocationReturned(returnedGpsLocation));
+
     }
 
     private void setLastGpsLocationReturned(GpsLocation returnedGpsLocation) {
         // callback
+    }
+
+    public void setHasArrivedAtDestLocation(boolean hasArrivedAtDestLocation) {
+        this.hasArrivedAtDestLocation = hasArrivedAtDestLocation;
     }
 }
