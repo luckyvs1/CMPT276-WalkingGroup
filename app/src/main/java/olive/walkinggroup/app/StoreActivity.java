6b981bf799c7dd763ac51b807c8388d8ef38b181
ckwan <ckw27@gmail.com>
2018-07-25 01:51:49 -0700

    Correctly shows user current points in Shop Activity

--- app/src/main/java/olive/walkinggroup/app/StoreActivity.java
+++ app/src/main/java/olive/walkinggroup/app/StoreActivity.java
@@ -2,16 +2,27 @@ package olive.walkinggroup.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import olive.walkinggroup.R;
+import olive.walkinggroup.dataobjects.Model;
+import olive.walkinggroup.dataobjects.User;
 
 public class StoreActivity extends AppCompatActivity {
+    private Model instance = Model.getInstance();
+    private User user = instance.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
+        setCurrentPoints();
+    }
 
+    private void setCurrentPoints(){
+        int currentPoints = user.getCurrentPoints();
+        TextView viewPoints = findViewById(R.id.shop_currentPoint);
+        viewPoints.append(" "+currentPoints);
     }
 }
