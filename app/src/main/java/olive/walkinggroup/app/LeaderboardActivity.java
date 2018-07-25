647d2ebbed0b15b327c7e94cda3cda5920a227f0
j_ho <joshuagjho@hotmail.com>
2018-07-24 21:53:42 -0700

    Added loading circle.

--- app/src/main/java/olive/walkinggroup/app/LeaderboardActivity.java
+++ app/src/main/java/olive/walkinggroup/app/LeaderboardActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.List;
 
@@ -54,9 +56,17 @@ public class LeaderboardActivity extends AppCompatActivity {
 
 
         populateUserList();
+        hideLoadingCircle();
 
 
+    }
 
+    private void hideLoadingCircle() {
+        RelativeLayout loadingCircle = findViewById(R.id.leaderboard_loading);
+
+        if (loadingCircle != null) {
+            loadingCircle.setVisibility(View.GONE);
+        }
     }
 
     private void populateUserList() {
