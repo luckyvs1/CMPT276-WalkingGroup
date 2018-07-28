633aff5cfab82daece3548a86e96798ef4754bcf
jgyeung <jgyeung@sfu.ca>
2018-07-27 23:03:47 -0700

    Update Permission UI to dynamic

--- app/src/main/java/olive/walkinggroup/app/ViewPermissionsActivity.java
+++ app/src/main/java/olive/walkinggroup/app/ViewPermissionsActivity.java
@@ -2,6 +2,7 @@ package olive.walkinggroup.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import olive.walkinggroup.R;
 
@@ -11,5 +12,12 @@ public class ViewPermissionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_permissions);
+
+        populatePermissionRequestList();
+    }
+
+    private void populatePermissionRequestList() {
+        ListView requestList = findViewById(R.id.viewPermissions_list);
+
     }
 }
