bb5370ee0129c42f26114964e061a40f3baccc4d
j_ho <joshuagjho@hotmail.com>
2018-07-30 22:00:22 -0700

    Refactored extracting title values into Rewards class.

--- app/src/main/java/olive/walkinggroup/dataobjects/Rewards.java
+++ app/src/main/java/olive/walkinggroup/dataobjects/Rewards.java
@@ -12,9 +12,12 @@ import olive.walkinggroup.R;
 
 public  class Rewards {
 
+    private Context context;
+
+
     public static final int NUM_TIERS = 10;
     public static final int NUM_ICONS_PER_TIER = 3;
-    private List<String> titles = new ArrayList<>(NUM_TIERS);
+    private List<String> titles;
     private List<List<Integer>> icons = new ArrayList<>(NUM_TIERS);
 
     public String getTierTitle(int pos){
@@ -26,10 +29,9 @@ public  class Rewards {
     }
 
     public Rewards(Context context){
-        TypedArray titlesArray = context.getResources().obtainTypedArray(R.array.titleNames);
-        for (int i = 0; i < NUM_TIERS; i++) {
-            titles.add(titlesArray.getString(i));
-        }
+
+        String[] titlesArray = context.getResources().getStringArray(R.array.titleNames);
+        titles = new ArrayList<>(Arrays.asList(titlesArray));
 
         TypedArray drawablesArray = context.getResources().obtainTypedArray(R.array.avatar_imgs);
 
